ea1671e21404da67f686429d1bdf07c93754bc6a
sseaver <sseaver321@gmail.com>
2016-09-19 13:08:14 -0400

    functions notes

---
+++ Week2/day1_functions.py
@@ -0,0 +1,12 @@
+
+def square(number):
+    return_value = number ** 2
+    print ("We got the value!")
+    return return_value
+
+print(square(94))
+
+def add(left, right):
+    return left + right
+
+print (add(12, 32))
