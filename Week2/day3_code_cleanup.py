f4353413fdd1eb8d72aec3d4814fb13681dfcd11
sseaver <sseaver321@gmail.com>
2016-09-21 10:53:02 -0400

    started refactoring

--- Week2/day3_code_cleanup.py
+++ Week2/day3_code_cleanup.py
@@ -4,14 +4,14 @@
 # Inventory
 # Items
 
-player = {"inventory": {}}
+player = {"inventory": {"red potion": {"quantity": 20}}}
 
 {
     "red potion": {
         "quantity": 1
     }
-
 }
+
 player_name = input("Welcome traveler! What do you call yourself?\n>")
 player["name"] = player_name
 print (player)
@@ -19,7 +19,11 @@ print (player)
 while True:
     choice = input("Would you like to (i)nspect your inventory? or (a)dd to your inventory?\n>")
     if choice == "a":
-        print ("Adding item to inventory")
+        item_name = input("What is this item called?\n>")
+        item_quantity = int(input("How many?\n>"))
+        player["inventory"][item_name] = {"quantity": item_quantity}
     elif choice == "i":
         print ("Looking at inventory")
-        print (player["inventory"])
+        for item in player["inventory"].keys():
+            quantity = player["inventory"][item]["quantity"]
+            print ("{} - {}".format(item, quantity))
