61bc9c48f1eae07fb96a10d84d3e18e29e0da6a8
sseaver <sseaver321@gmail.com>
2016-10-11 12:09:14 -0400

    can submit chirps

---
+++ Week5/chirper2/app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+from app.models import Chirp
+# Create your views here.
+
+
+def index_view(request):
+    print(request.POST)
+    if request.POST:
+        chirp_body = request.POST["chirp_body"]
+        if chirp_body != "":
+            Chirp.objects.create(body=chirp_body)
+    context = {
+        "all_chirps": Chirp.objects.all().order_by("-created")
+    }
+    return render(request, "index.html", context)
+
+
+def about_view(request):
+
+    return render(request, "about.html")
