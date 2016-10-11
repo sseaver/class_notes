1cc0823dfc4c233a2d328e5f1dd81e27b1e18d14
sseaver <sseaver321@gmail.com>
2016-10-11 09:30:18 -0400

    added chirper day1

---
+++ Week5/chirper/app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index_view(request):
+
+    return render(request, "index.html")
+
+
+def favorites_view(request):
+
+    return render(request, "favorites.html")
